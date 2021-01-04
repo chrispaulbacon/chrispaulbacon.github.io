5f268aa211fe6ecb540f50ee2ab8ad2e49a5ac06
jobvillanca <76563339+jobvillanca@users.noreply.github.com>
2021-01-04 02:32:35 -0500

    added username and password form

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 console.log("Hey I'm Chris P. Bacon")
 
+
+const resultsList = document.getElementById('results')
+new URLSearchParams(window.location.search).forEach((value,name)
+    => {
+        resultsList.append(`${name}: ${value}`)
+        resultsList.append(document.createElement('br'))
+    })
